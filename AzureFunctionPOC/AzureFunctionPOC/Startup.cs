de1c3e72baff9da81861dec15486cfce8fb0eca1
Helder Boone Borges <Helder.Borges@ey.com>
2022-09-15 19:58:54 -0300

    Set startup class

---
+++ AzureFunctionPOC/AzureFunctionPOC/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(AzureFunctionPOC.Startup))]
+namespace AzureFunctionPOC;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+    }
+}
