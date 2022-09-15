276a4f2f99a1924fcea407f4975c06a505317d5d
Helder Boone Borges <Helder.Borges@ey.com>
2022-09-15 19:59:29 -0300

    Create first method to send message to Service Bus

---
+++ AzureFunctionPOC/SenderMessagePOC/Program.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenderMessagePOC
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await ServiceBusSenderHelper.SendMessage();
+        }
+    }
+
+    public class ServiceBusSenderHelper
+    {
+        public static async Task SendMessage()
+        {
+            try
+            {
+                while (true)
+                {
+                    string connectionString = "";
+                    string topicName = "";
+
+                    await using var client = new ServiceBusClient(connectionString);
+
+                    ServiceBusSender sender = client.CreateSender(topicName);
+
+                    var removeShoppingCartMessage = new RemoveShoppingCartMessage
+                    {
+                        CustomerId = Guid.NewGuid(),
+                    };
+
+                    var json = JsonSerializer.Serialize(removeShoppingCartMessage);
+
+                    var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(json));
+                    var sessionId = Guid.NewGuid().ToString();
+                    message.SessionId = sessionId;
+                    //message.MessageId = Guid.NewGuid().ToString();
+
+                    await Task.Delay(500);
+                    await sender.SendMessageAsync(message);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ AzureFunctionPOC/SenderMessagePOC/RemoveShoppingCartMessage.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SenderMessagePOC
+{
+    public class RemoveShoppingCartMessage
+    {
+        public Guid CustomerId { get; set; }
+    }
+}
