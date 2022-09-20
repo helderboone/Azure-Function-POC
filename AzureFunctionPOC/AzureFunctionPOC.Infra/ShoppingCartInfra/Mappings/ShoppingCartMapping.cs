02d0dd08eb5f7fa26c7fcb782a35b1e1de57579a
Helder Boone Borges <Helder.Borges@ey.com>
2022-09-20 14:12:46 -0300

    Create migration for shopping cart, car item and voucher

---
+++ AzureFunctionPOC/AzureFunctionPOC.Infra/ShoppingCartInfra/Mappings/ShoppingCartMapping.cs
@@ -0,0 +1,31 @@
+﻿using AzureFunctionPOC.Domain.ShoppingCart;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace AzureFunctionPOC.Infra.ShoppingCartInfra.Mappings;
+
+public class ShoppingCartMapping : IEntityTypeConfiguration<ShoppingCart>
+{
+    public void Configure(EntityTypeBuilder<ShoppingCart> builder)
+    {
+        builder.HasIndex(x => x.CustomerId).HasName("IDX_Customer");
+
+        builder.Ignore(x => x.Voucher)
+            .OwnsOne(x => x.Voucher, v =>
+            {
+                v.Property(vc => vc.Code).HasColumnType("varchar(50)");
+
+                v.Property(vc => vc.DiscountType);
+
+                v.Property(vc => vc.Percentage);
+
+                v.Property(vc => vc.Discount);
+            });
+
+        //EF Relationship
+        builder.HasMany(sc => sc.Items)
+                .WithOne(i => i.ShoppingCart)
+                .HasForeignKey(ci => ci.ShoppingCartId);
+    }
+}
---
+++ AzureFunctionPOC/AzureFunctionPOC.Infra/ShoppingCartInfra/ShoppingCartContext.cs
@@ -0,0 +1,41 @@
+﻿using AzureFunctionPOC.Domain.ShoppingCart;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace AzureFunctionPOC.Infra.ShoppingCartInfra;
+
+public class ShoppingCartContext : DbContext
+{
+    public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
+            : base(options)
+    {
+    }
+
+    /// <summary>
+    /// Display output query sql
+    /// </summary>
+    /// <param name="optionsBuilder"></param>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(message => Debug.WriteLine(message));
+    }
+
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<ShoppingCart> ShoppingCart { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+            e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            property.SetColumnType("varchar(100)");
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.Cascade;
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
