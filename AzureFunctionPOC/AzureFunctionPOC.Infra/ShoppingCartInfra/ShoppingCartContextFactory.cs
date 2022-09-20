69d57aaea0f8f7abc5994e0c35facc611bab7c42
Helder Boone Borges <Helder.Borges@ey.com>
2022-09-20 10:43:14 -0300

    Implement create db context in order to run migrations on az func

---
+++ AzureFunctionPOC/AzureFunctionPOC.Infra/ShoppingCartInfra/ShoppingCartContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AzureFunctionPOC.Infra.ShoppingCartInfra
+{
+    public class ShoppingCartContextFactory : IDesignTimeDbContextFactory<ShoppingCartContext>
+    {
+        public ShoppingCartContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ShoppingCartContext>();
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("ShoppingCartConnString"));
+
+            return new ShoppingCartContext(optionsBuilder.Options);
+        }
+    }
+}
