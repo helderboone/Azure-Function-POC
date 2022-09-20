ef8a147fdc9d7d594ebad6a2a6086ffc009454d9
Helder Boone Borges <Helder.Borges@ey.com>
2022-09-20 10:38:48 -0300

    Set ef core dependecy injection

--- AzureFunctionPOC/AzureFunctionPOC/Startup.cs
+++ AzureFunctionPOC/AzureFunctionPOC/Startup.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using AzureFunctionPOC.Infra.ShoppingCartInfra;
+using Microsoft.Azure.Amqp.Framing;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 [assembly: FunctionsStartup(typeof(AzureFunctionPOC.Startup))]
 namespace AzureFunctionPOC;
@@ -7,5 +12,10 @@ public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
+        var configuration = builder.GetContext().Configuration;
+
+        string connectionString = Environment.GetEnvironmentVariable("ShoppingCartConnString");
+        builder.Services.AddDbContext<ShoppingCartContext>(options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString));
+
     }
 }
